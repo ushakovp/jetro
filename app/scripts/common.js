55f4b8081f7fb950c63639c726a78b993fbcc483
unknown <E:\serg\thebat>
2015-04-06 13:18:48 +0400

    Slider

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -3,5 +3,17 @@ $(function () {
 	'use strict';
 
 	// Code here
+$(document).ready(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+});
 
 });
