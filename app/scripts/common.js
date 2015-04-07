e1fdd831cd7bbe967d21266f72208217dbc82615
unknown <E:\serg\thebat>
2015-04-07 09:54:11 +0400

    Swiper removed

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -3,17 +3,5 @@ $(function () {
 	'use strict';
 
 	// Code here
-$(document).ready(function () {
-    var mySwiper = new Swiper ('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: true,
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false
-    });
-});
 
 });
