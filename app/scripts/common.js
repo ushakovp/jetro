e9679c983e6942ca807e0ae18296d07ae8104d86
ushakovp <ushakovpb@gmail.com>
2015-04-14 13:09:47 +0400

    Slider

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -6,10 +6,28 @@ $(function () {
 	$(document).ready(function(){
 		$('.bxslider').bxSlider({
 			auto: true,
+			minSlides: 1,
+			maxSlides: 1,
+			slideWidth: 940,
 			nextSelector: ".slider-controls-next",
 			prevSelector: ".slider-controls-prev",
-			nextText: "Suka",
-			prevText: "pzdc"
+			buildPager: function(slideIndex){
+				switch(slideIndex){
+					case 0:
+						return '<img src="../../assets/images/slider/1.png">';
+					case 1:
+						return '<img src="../../assets/images/slider/2.png">';
+					case 2:
+						return '<img src="../../assets/images/slider/3.png">';
+					case 3:
+						return '<img src="../../assets/images/slider/4.png">';
+					case 4:
+						return '<img src="../../assets/images/slider/5.png">';
+					case 5:
+						return '<img src="../../assets/images/slider/6.png">';
+				}
+			}
+
 		});
 	});
 
